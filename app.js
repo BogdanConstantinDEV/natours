415cf38c43c7167480431d93e38d009239508ae4
BogdanConstantin92 <bogdan_porojan@yahoo.com>
2020-02-22 08:21:42 +0200

    added fail route

--- app.js
+++ app.js
@@ -9,4 +9,27 @@ app.use(express.json())
 // routes
 app.use('/api/v1/tours', tourRouter)
 
+
+// fail route
+app.use('*', (req, res, next) => {
+    const err = new Error(`Can't find anything at ${req.originalUrl}`)
+    err.statusCode = 404
+    err.status = 'fail'
+
+    next(err)
+})
+
+
+// fail middleware
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500
+    err.status = err.status || 'fail'
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    })
+})
+
+
 module.exports = app
