1d0b1e0878d73c0684fb39b13d44a7e2beaa4cbd
BogdanConstantin92 <bogdan_porojan@yahoo.com>
2020-02-22 11:45:21 +0200

    app error handler refactored

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const tourRouter = require('./routes/tourRoutes')
+const globalErrorHandler = require('./controllers/errorController')
+const AppError = require('./util/appError')
 
 const app = express()
 
@@ -10,26 +12,16 @@ app.use(express.json())
 app.use('/api/v1/tours', tourRouter)
 
 
-// fail route
-app.use('*', (req, res, next) => {
-    const err = new Error(`Can't find anything at ${req.originalUrl}`)
-    err.statusCode = 404
-    err.status = 'fail'
-
-    next(err)
+// error function for unknown requests
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't access ${req.originalUrl} on this server`, 404))
 })
 
 
-// fail middleware
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500
-    err.status = err.status || 'fail'
+// error middleware
+app.use(globalErrorHandler)
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    })
-})
+
 
 
 module.exports = app
