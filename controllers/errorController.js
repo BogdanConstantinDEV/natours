b5eba15874d272555407963af8e110e6221a3c8d
BogdanConstantin92 <bogdan_porojan@yahoo.com>
2020-02-25 16:45:22 +0200

    added duplicate key and validation error handlers

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,21 @@
 const AppError = require('../util/appError')
 
 
-
+// handle wrong DB id error
 const handleCastError = err => {
     return new AppError(`Invalid ${err.path}: ${err.value} 💥`, 400)
+}
+
+// handle duplicate key error
+const handleDuplicateError = err => {
+    const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0]
+    return new AppError(`Duplicate field value: ${value}😉`, 400)
+}
+
+// handle validation error
+const handleValidationError = err => {
+    const errorMessages = Object.values(err.errors).map(el => el.message)
+    return new AppError(`Invalid fields: ${errorMessages.join(' 👀  ')}`, 400)
 }
 
 const sendErrorDev = (err, res) => {
@@ -38,7 +50,15 @@ module.exports = (err, req, res, next) => {
     }
     else if (process.env.NODE_ENV === 'production') {
         let error = { ...err }
+
+        // wrong db id
         if (error.name === 'CastError') error = handleCastError(error)
+
+        // duplicate key error
+        if (error.code === 11000) error = handleDuplicateError(error)
+
+        // valuidation error
+        if (error.name = 'ValidationError') error = handleValidationError(error)
 
         sendErrorProd(error, res)
     }
