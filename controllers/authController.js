83f37b5d4e91b0d27a2ffd870e92807cea722775
BogdanConstantin92 <bogdan_porojan@yahoo.com>
2020-02-27 20:13:56 +0200

    user signup bug fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,12 @@ const catchAsync = require('../util/catchAsync')
 
 // sign up
 exports.signUp = catchAsync(async (req, res) => {
-    const user = await UserModel.create(req.body)
+    const user = await UserModel.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    })
     res.status(201).json({
         status: 'success',
         data: {
