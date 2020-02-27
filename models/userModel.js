81d6d2e64fdad647ae235dda379280573760fe73
BogdanConstantin92 <bogdan_porojan@yahoo.com>
2020-02-27 19:45:06 +0200

    added password encryption

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema(
     {
@@ -16,7 +17,8 @@ const userSchema = new mongoose.Schema(
             trim: true,
             unique: true,
             required: [true, 'user must have a email'],
-            validate: [validator.isEmail, 'use a valid email']
+            validate: [validator.isEmail, 'use a valid email'],
+            lowercase: true
         },
         photo: {
             type: String
@@ -32,12 +34,25 @@ const userSchema = new mongoose.Schema(
             validate: {
                 validator: function (val) {
                     return val === this.password
-                }
-            },
-            message: 'password should match'
+                },
+                message: 'password should match'
+            }
         }
     }
 )
+
+
+// encrypt password
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next()
+
+    this.password = await bcrypt.hash(this.password, 12)
+    this.passwordConfirm = undefined
+
+    next()
+})
+
+
 
 const User = new mongoose.model('User', userSchema)
 module.exports = User
