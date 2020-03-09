c4224004cd269fd6240a18e953dff9b9aa7de8ec
BogdanConstantin92 <bogdan_porojan@yahoo.com>
2020-03-10 00:10:18 +0200

    send jwt token via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,6 +19,15 @@ const signToken = id => {
 // create and send token function
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id)
+
+    // send token via cookie
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    }
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true
+    res.cookie('jwt', token, cookieOptions)
+
     res.status(statusCode).json({
         status: 'success',
         token
