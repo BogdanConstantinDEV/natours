cfbe8f4971db919461aa1538805da70e3a41248b
BogdanConstantin92 <bogdan_porojan@yahoo.com>
2020-03-09 12:26:06 +0200

    added user update data feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,67 @@
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined'
+const UserModel = require('../models/userModel')
+const catchAsync = require('../util/catchAsync')
+const AppError = require('../util/appError')
+
+
+
+// filter body object
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {}
+    Object.keys(obj).forEach(el => {
+        if (allowedFields.includes(el)) newObj[el] = obj[el]
     })
+
+    return newObj
 }
+
+
+
+
+
+
+// get all users
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+
+    const users = await UserModel.find()
+
+    res.status(200).json({
+        status: 'success',
+        results: users.length,
+        data: users
+    })
+})
+
+
+
+
+
+// update user data
+exports.updateMe = catchAsync(async (req, res, next) => {
+
+    // check if user is trying to update password
+    if (req.body.password || req.body.passwordConfirm) {
+        return next(new AppError('You are not allowed to update your password! If you want to update your password, do it here /update-my-password 😉', 400))
+    }
+
+    // update user data
+    const filteredBody = filterObj(req.body, 'name', 'email')
+    const user = await UserModel.findByIdAndUpdate(req.user._id, filteredBody, {
+        new: true,
+        runValidators: true
+    })
+
+    // return new user
+    res.status(200).json({
+        status: 'success',
+        data: user
+    })
+})
+
+
+
+
+
+
 exports.createUser = (req, res) => {
     res.status(500).json({
         status: 'error',
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,16 +6,24 @@ const authController = require('../controllers/authController')
 
 const router = express.Router()
 
-// sign up & log in
+// sign up/log-in/update-my-password
 router.post('/signup', authController.signUp)
 router.post('/login', authController.login)
 
 // forgot/restart/update password
 router.post('/forgot-password', authController.forgotPassword)
 router.patch('/reset-password/:token', authController.resetPassword)
-router.patch('/update-password',
+router.patch('/update-my-password',
     authController.protect,
     authController.updatePassword
+)
+
+
+
+// update user data
+router.patch('/update-me',
+    authController.protect,
+    userController.updateMe
 )
 
 
