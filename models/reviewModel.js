89816d171e700e9021344689ed1db37cd5c06fb9
BogdanConstantin92 <bogdan_porojan@yahoo.com>
2020-03-27 20:37:10 +0200

    prevent duplicate reviews and round up ratingsAverage

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -37,6 +37,10 @@ const reviewSchema = new mongoose.Schema(
 )
 
 
+// prevent duplicate reviews
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true })
+
+
 
 // << QUERY MIDDLEWARE >>
 reviewSchema.pre(/^find/, function (next) {
